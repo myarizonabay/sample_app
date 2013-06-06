8033cb92069ca2b43fe386cb427687865de70037
myarizonabay <arizona@blueyonder.co.uk>
2013-06-06 13:02:10 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5484b2561896cd4d8ee6bc1957c2f65ba171c9d2fa315a97bf462946c674551e7a7a3482e30d151a24650a7e8482e74bf86f243de9e5a4400cfa8b1b704e34c3'
